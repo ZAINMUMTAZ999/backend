3b368e6e52de61c09878f3f2fbf7ddebc5af696b
ZAINMUMTAZ999 <mzainmumtaz99@gmail.com>
2025-10-21 01:37:58 +0500

    first commit

---
+++ netlify/functions/api.ts
@@ -0,0 +1,68 @@
+import express,{Router,Request, Response} from "express";
+import serverless from "serverless-http";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import { registerRouter } from "../../src/routes/registerRouter";
+import { loginRouter } from "../../src/routes/loginRouter";
+import { v2 as cloudinary } from "cloudinary";
+import { addHeroImageRouter } from "../../src/routes/addHeroImageRouter";
+
+const MONGODB_URL = process.env.MONGODB_URL as string;
+
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log("Database connected successfully"))
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
+  // Allowed origins (add as many as you want)
+const allowedOrigins = [
+  "https://demosekaispacehotelapp.vercel.app"
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like Postman or mobile apps)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+// app.use(
+//   cors({
+//     origin: "https://demosekaispacehotelapp.vercel.app",
+ 
+
+//     credentials: true,
+//   })
+// );
+// 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+const router = Router();
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+router.get("/", (_req, res: Response) => {
+  res.send("✅ Backend running successfully on Netlify!");
+});
+
+router.use("/v1", registerRouter);
+router.use("/v2", loginRouter);
+router.use("/v3", addHeroImageRouter);
+
+app.use("/", router);
+export const handler = serverless(app);
